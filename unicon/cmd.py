bc55dc9383f1322f62f9fee57f6f54bef1c9aede
Hyungro Lee <hroe.lee@gmail.com>
2015-08-26 22:35:26 -0400

    yaml default

--- unicon/cmd.py
+++ unicon/cmd.py
@@ -5,10 +5,16 @@ def cli():
     pass
 
 @cli.command('create')
-@click.argument('app')
-def create(app):
+@click.argument('name')
+def create(name):
     """Creates a new cluster"""
-    click.echo("%s cluster" % app)
+    click.echo("%s cluster" % name)
 
+@cli.command('list')
+@click.argument('name')
+def list(name):
+    """Lists clusters"""
+    click.echo("mesos")
+    # list of clusters from yaml 
 
 
