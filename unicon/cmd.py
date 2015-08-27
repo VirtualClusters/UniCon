16380370fed052b9a460967bc2e1c4625c0a1ba0
Hyungro Lee <hroe.lee@gmail.com>
2015-08-27 15:42:36 -0400

    working on registering resources and updating it when its necessary

--- unicon/cmd.py
+++ unicon/cmd.py
@@ -1,5 +1,5 @@
 import click
-import unicon.data as ud
+import unicon.data as udata
 
 @click.group()
 def cli():
@@ -10,12 +10,59 @@ def cli():
 def create(name):
     """Creates a new cluster"""
     click.echo("%s cluster" % name)
-    click.echo(ud.read_cluster(name))
+    click.echo(udata.read_cluster(name))
 
 @cli.command('list')
-def list():
-    """Lists clusters"""
-    click.echo(ud.clusters())
-    # list of clusters from yaml 
+@click.argument('name', default='cluster')
+def list(name):
+    """Lists clusters or resources"""
+    if name == "cluster":
+        # list of clusters from yaml 
+        dlist = list(enumerate(udata.clusters(), start=1))
+    elif name == "resource":
+        dlist = list(enumerate(udata.resources(), start=1))
+    else:
+        print ("Unexpected type")# %s" % name)
+        dlist=None
+
+    click.echo(dlist)
+
+@cli.command('register')
+@click.argument('name')
+def register(name):
+    """Registers clusters or resources"""
+    if name == "cluster":
+        click.echo("hello registering cluster")
+    elif name == "resource":
+        register_resource(name)
+    else:
+        click.echo("not supported")
+
+def register_resource(name):
+    rtype = click.prompt("1) bare metal, 2) IaaS", type=int)
+    if rtype == 1:
+        click.echo("Cobbler or PXE Boot will be configured (TBD)")
+    elif rtype == 2:
+        click.echo("Provide IaaS Credentials (End with 'EOF')")
+        #credential = click.get_text_stream('stdin')
+        sentinel = 'EOF'
+        cred = '\n'.join(iter(raw_input, sentinel))
+        #click.echo(creds)
+        if click.confirm("Cert file?"):
+            cert = '\n'.join(iter(raw_input, sentinel))
+        rname = click.prompt("Resource name? (e.g. futuresystems," \
+                + "chameleon, AWS", type=str)
+        udata.write_resource(rname, "cred", cred)
+        udata.write_resource(rname, "cert", cert)
+
+@cli.command('update')
+@click.argument('name')
+def update(name):
+    """Updates clusters or resources"""
+    if name == "cluster":
+        # list of clusters from yaml 
+        click.echo(udata.clusters())
+    elif name == "resource":
+        click.echo(udata.resources())
 
 
--- unicon/data.py
+++ unicon/data.py
@@ -1,17 +1,52 @@
-from os.path import expanduser, basename, splitext
+import os
 from glob import glob
 import yaml
 
-BASE_DIR=expanduser("~") + "/.unicon"
+BASE_DIR=os.path.expanduser("~") + "/.unicon"
 
 def clusters():
-    res = glob(BASE_DIR + "/cluster/*.yaml")
-    return [splitext(basename(i))[0] for i in res]
+    return list_clusters()
+
+def list_clusters():
+    return get_list_of_files(BASE_DIR + "/cluster/*.yaml")
+
+def resources():
+    return list_resources()
+
+def list_resources():
+    return get_list_of_files(BASE_DIR + "/resource/*")
+
+def get_list_of_files(path):
+    res = glob(path)
+    return [os.path.splitext(os.path.basename(i))[0] for i in res]
 
 def read(name):
-    stream = file(BASE_DIR + name, 'r')
+    stream = file(name, 'r')
     return yaml.load(stream)
+    # READ FROM TEXT
 
 def read_cluster(name):
-    return read("/cluster/" + name + ".yaml")
+    return read(BASE_DIR + "/cluster/" + name + ".yaml")
 
+def write(filepath, data):
+    directory = os.path.dirname(filepath)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    f = open(filepath, 'w')
+    f.write(data)
+    f.write('\n')
+    f.close()
+
+def write_resource(name, ftype, text):
+    if ftype == "cred":
+        filename = ".cred"
+    elif ftype == "cert":
+        filename = name + ".pem"
+    else:
+        print ("Unexpected type")
+        raise
+
+    # From BASE_DIR
+    filepath = (BASE_DIR + "/resource/" + name + "/" + filename)
+    write(filepath, text)
+    os.chmod(filepath, 0600)
